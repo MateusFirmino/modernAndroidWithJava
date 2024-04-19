ad3fe876c5626120f6584ea77fea94f1eb92de72
Mateus Firmino <C0653675@vale.com>
2024-04-19 11:29:41 -0300

    new project - modern android app using java, mvvm, dagger2, RxJava, Retrofit and etc...

---
+++ app/src/main/java/com/devtides/countriesapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.devtides.countriesapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
